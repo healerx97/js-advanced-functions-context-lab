57fbefdc264aa837ed9d7234161cf4c056271344
Kenny Yoon <ghun.yoon@gmail.com>
2021-06-01 04:34:25 -0400

    commit tree a1e9f23b518e0b94a1674730ff3c45ed8027997e

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-/* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payRate]) {
+    let record = {
+        "firstName" : firstName,
+        "familyName" : familyName,
+        "title" : title,
+        "payPerHour" : payRate,
+        "timeInEvents" : [],
+        "timeOutEvents" : []
+    }
+    return record
+}
+function createEmployeeRecords(records) {
+    return records.map(record=> createEmployeeRecord(record))
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+let createTimeInEvent = function(events) {
+    let obj = {
+        'type' : 'TimeIn',
+        'hour' : events.substring(events.length()-4, events.length()),
+        'date' : events.substring(0,10)
+    }
+    this.timeInEvents.push(obj)
+    return this
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+
 
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
